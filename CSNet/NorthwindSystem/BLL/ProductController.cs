a595e1cfbacc4e36ed4aafc22797ac4445049c0d
abucog2nait <46579732+abucog2nait@users.noreply.github.com>
2019-04-04 13:46:32 -0600

    updated

--- CSNet/NorthwindSystem/BLL/ProductController.cs
+++ CSNet/NorthwindSystem/BLL/ProductController.cs
@@ -81,5 +81,98 @@ namespace NorthwindSystem.BLL
                 return results.ToList();
             }
         }
-    }
-}
+        public List<Product> Products_GetByPartialProductName(string partialname)
+        {
+            using (var context = new NorthwindContext())
+            {
+                IEnumerable<Product> results =
+                    context.Database.SqlQuery<Product>("Products_GetByPartialProductName @PartialName",
+                                    new SqlParameter("PartialName", partialname));
+                return results.ToList();
+            }
+        }
+
+      
+
+        public List<Product> Products_GetBySupplierPartialProductName(int supplierid, string partialproductname)
+        {
+            using (var context = new NorthwindContext())
+            {
+                //sometimes there may be a sql error that does not like the new SqlParameter()
+                //       coded directly in the SqlQuery call
+                //if this happens to you then code your parameters as shown below then
+                //       use the parm1 and parm2 in the SqlQuery call instead of the new....
+                //don't know why but its weird
+                //var parm1 = new SqlParameter("SupplierID", supplierid);
+                //var parm2 = new SqlParameter("PartialProductName", partialproductname);
+                IEnumerable<Product> results =
+                    context.Database.SqlQuery<Product>("Products_GetBySupplierPartialProductName @SupplierID, @PartialProductName",
+                                    new SqlParameter("SupplierID", supplierid),
+                                    new SqlParameter("PartialProductName", partialproductname));
+                return results.ToList();
+            }
+        }
+
+        public List<Product> Products_GetForSupplierCategory(int supplierid, int categoryid)
+        {
+            using (var context = new NorthwindContext())
+            {
+                IEnumerable<Product> results =
+                    context.Database.SqlQuery<Product>("Products_GetForSupplierCategory @SupplierID, @CategoryID",
+                                    new SqlParameter("SupplierID", supplierid),
+                                    new SqlParameter("CategoryID", categoryid));
+                return results.ToList();
+            }
+        }
+
+        public List<Product> Products_GetByCategoryAndName(int category, string partialname)
+        {
+            using (var context = new NorthwindContext())
+            {
+                IEnumerable<Product> results =
+                    context.Database.SqlQuery<Product>("Products_GetByCategoryAndName @CategoryID, @PartialName",
+                                    new SqlParameter("CategoryID", category),
+                                    new SqlParameter("PartialName", partialname));
+                return results.ToList();
+            }
+        }
+
+        #region Add, Update and Delete of CRUD
+
+        //The Add method will be used to insert a product instance into the database
+        //This method will recieve an instance of Product
+        //This method can optionally return the new identity primary key
+        public int Product_Add(Product item)
+        {
+            //the addition of the data will be done in a transaction block
+            using (var context = new NorthwindContext())
+            {
+
+                //Step 1: Staging
+                //one adds the new instance to the appropriate DbSet<T>
+                //the data needs to be in an instance of <T> 
+                //staging does NOT place the record on the database
+                //if the primary key of the <T> is an identity type
+                //    the pkey value is NOT yet set
+                context.Products.Add(item);
+
+                //step 2: Committing transaction
+                //if the command to save your DbSet changes is NOT executed
+                //   the transaction fails and a RollBack is performed
+                //if the command to save your DbSet changes is executed and fails
+                //   the transaction is RollBacked and the appropriate error message
+                //   is issued
+                //at this point ANY entity validation is executed
+                //if the command to save your DbSet changes is successful then the
+                //   data is in the database (unless the database finds an exception)
+                //at this point your new identity pkey value is present in your
+                //   <T> instance and can be retrieved
+                context.SaveChanges();
+
+                //optionally you can return the new pKey value
+                return item.ProductID;
+            }
+        }
+        #endregion
+    }//eoc
+}//eon
